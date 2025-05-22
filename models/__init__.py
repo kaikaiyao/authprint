40d7d8c9f24232883406dcca8e777055e539f878
Kai Yao <kai.yao@ed.ac.uk>
2025-05-22 18:13:01 +0100

    mod

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,7 @@
 Models module for StyleGAN Fingerprinting.
 """
 
-from .decoder import Decoder
+from .decoder import DecoderSD_L, DecoderSD_M, DecoderSD_S, StyleGAN2Decoder
 from .model_utils import load_stylegan2_model, clone_model
 
-__all__ = ["Decoder", "load_stylegan2_model", "clone_model"] 
+__all__ = ["DecoderSD_L", "DecoderSD_M", "DecoderSD_S", "StyleGAN2Decoder", "load_stylegan2_model", "clone_model"] 
